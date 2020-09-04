3c41007bbf7465544254510217c823f4c0d21acb
Tahlor Perez <tahlor.perez@soaringsoftware.com>
2020-09-04 09:59:41 -0400

    done

---
+++ src/app/admin-guard.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuardGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+      //write authentication and authorization code here
+      //calling user service- pass password and username to make sure user is correct
+    return false;
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DashboardProfileComponent } from './dashboard-profile/dashboard-profile.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { AdminGuardGuard } from './admin-guard.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full'}, //redirect
   { path: 'welcome', component: WelcomePageComponent},
-  { path: 'contact-us', component: ContactUsComponent},
+  { path: 'contact-us', component: ContactUsComponent }, 
   { path: 'info', component: InfoComponent},
   { path: 'info/:id', component: InfoComponent}, //parameterized route
-  { path: 'dashboard', 
+  { path: 'dashboard', canActivate: [AdminGuardGuard], //route guard is false, cant access, change to true if wanted
     children: [ //child routes
       { path: 'home', component: DashboardComponent},
       { path: 'profile', component: DashboardProfileComponent},
